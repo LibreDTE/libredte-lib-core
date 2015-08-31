921b47461337888f0046c0e4b956d7d7f044498f
Esteban De La Fuente Rubio <esteban@delaf.cl>
2015-08-31 14:52:42 -0300

    Se agrega clase para cargar, verificar y extraer datos del CAF

---
+++ examples/007-folios.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * LibreDTE
+ * Copyright (C) SASCO SpA (https://sasco.cl)
+ *
+ * Este programa es software libre: usted puede redistribuirlo y/o
+ * modificarlo bajo los términos de la Licencia Pública General GNU
+ * publicada por la Fundación para el Software Libre, ya sea la versión
+ * 3 de la Licencia, o (a su elección) cualquier versión posterior de la
+ * misma.
+ *
+ * Este programa se distribuye con la esperanza de que sea útil, pero
+ * SIN GARANTÍA ALGUNA; ni siquiera la garantía implícita
+ * MERCANTIL o de APTITUD PARA UN PROPÓSITO DETERMINADO.
+ * Consulte los detalles de la Licencia Pública General GNU para obtener
+ * una información más detallada.
+ *
+ * Debería haber recibido una copia de la Licencia Pública General GNU
+ * junto a este programa.
+ * En caso contrario, consulte <http://www.gnu.org/licenses/gpl.html>.
+ */
+
+/**
+ * @file 007-folios.php
+ *
+ * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
+ * @version 2015-08-31
+ */
+
+// respuesta en texto plano
+header('Content-type: text/plain; charset=ISO-8859-1');
+
+// incluir archivos php de la biblioteca y configuraciones
+include 'inc.php';
+
+// cargar folios
+$Folios = new \sasco\LibreDTE\Sii\Folios(file_get_contents('folios.xml'));
+
+// ejemplos métodos
+echo 'Folios son validos?: ',($Folios->check()?'si':'no'),"\n\n";
+echo 'Rango de folios: ',$Folios->getDesde(),' al ',$Folios->getHasta(),"\n\n";
+echo 'CAF: ',$Folios->getCaf(),"\n\n";
+echo $Folios->getPrivateKey(),"\n";
+echo $Folios->getPublicKey();
---
+++ lib/Sii/Folios.php
@@ -0,0 +1,167 @@
+<?php
+
+/**
+ * LibreDTE
+ * Copyright (C) SASCO SpA (https://sasco.cl)
+ *
+ * Este programa es software libre: usted puede redistribuirlo y/o
+ * modificarlo bajo los términos de la Licencia Pública General GNU
+ * publicada por la Fundación para el Software Libre, ya sea la versión
+ * 3 de la Licencia, o (a su elección) cualquier versión posterior de la
+ * misma.
+ *
+ * Este programa se distribuye con la esperanza de que sea útil, pero
+ * SIN GARANTÍA ALGUNA; ni siquiera la garantía implícita
+ * MERCANTIL o de APTITUD PARA UN PROPÓSITO DETERMINADO.
+ * Consulte los detalles de la Licencia Pública General GNU para obtener
+ * una información más detallada.
+ *
+ * Debería haber recibido una copia de la Licencia Pública General GNU
+ * junto a este programa.
+ * En caso contrario, consulte <http://www.gnu.org/licenses/gpl.html>.
+ */
+
+namespace sasco\LibreDTE\Sii;
+
+/**
+ * Clase para realizar operaciones con lo Folios autorizados por el SII
+ * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
+ * @version 2015-08-31
+ */
+class Folios
+{
+
+    private $xml; ///< Objeto XML que representa el CAF
+
+    /**
+     * Constructor de la clase
+     * @param xml Datos XML del código de autorización de folios (CAF)
+     * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
+     * @version 2015-08-19
+     */
+    public function __construct($xml)
+    {
+        $this->xml = new \sasco\LibreDTE\XML();
+        $this->xml->loadXML($xml);
+        if (!$this->check())
+            $this->xml = null;
+    }
+
+    /**
+     * Método que verifica el código de autorización de folios
+     * @return =true si está ok el XML cargado
+     * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
+     * @version 2015-08-31
+     */
+    public function check()
+    {
+        // validar firma del SII sobre los folios
+        $firma = base64_decode($this->getFirma());
+        $pub_key = \sasco\LibreDTE\Sii::cert($this->getIDK());
+        if (!openssl_public_decrypt($firma, $digest_firmado, $pub_key))
+            return false;
+        $digest = base64_encode(sha1($this->xml->getFlattened('/AUTORIZACION/CAF/DA'), true));
+        if ($digest !== substr(base64_encode($digest_firmado), -strlen($digest)))
+            return false;
+        // validar clave privada y pública proporcionada por el SII
+        $plain = md5(date('U'));
+        if (!openssl_private_encrypt($plain, $crypt, $this->getPrivateKey()))
+            return false;
+        if (!openssl_public_decrypt($crypt, $plain_firmado, $this->getPublicKey()))
+            return false;
+        return $plain === $plain_firmado;
+    }
+
+    /**
+     * Método que entrega el nodo CAF
+     * @return DomElement
+     * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
+     * @version 2015-08-31
+     */
+    public function getCaf()
+    {
+        if (!$this->xml)
+            return false;
+        return $this->xml->getFlattened('/AUTORIZACION/CAF');
+    }
+
+    /**
+     * Método que entrega el primer folio autorizado en el CAF
+     * @return Número del primer folio
+     * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
+     * @version 2015-08-19
+     */
+    public function getDesde()
+    {
+        if (!$this->xml)
+            return false;
+        return (int)$this->xml->getElementsByTagName('D')->item(0)->nodeValue;
+    }
+
+    /**
+     * Método que entrega el últimmo folio autorizado en el CAF
+     * @return Número del último folio
+     * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
+     * @version 2015-08-19
+     */
+    public function getHasta()
+    {
+        if (!$this->xml)
+            return false;
+        return (int)$this->xml->getElementsByTagName('H')->item(0)->nodeValue;
+    }
+
+    /**
+     * Método que entrega la firma del SII sobre el nodo DA
+     * @return Firma en base64
+     * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
+     * @version 2015-08-30
+     */
+    private function getFirma()
+    {
+        if (!$this->xml)
+            return false;
+        return $this->xml->getElementsByTagName('FRMA')->item(0)->nodeValue;
+    }
+
+    /**
+     * Método que entrega el IDK (serial number) de la clave pública del SII
+     * utilizada para firmar el CAF
+     * @return Serial number
+     * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
+     * @version 2015-08-31
+     */
+    private function getIDK()
+    {
+        if (!$this->xml)
+            return false;
+        return (int)$this->xml->getElementsByTagName('IDK')->item(0)->nodeValue;
+    }
+
+    /**
+     * Método que entrega la clave privada proporcionada por el SII para el CAF
+     * @return Clave privada en base64
+     * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
+     * @version 2015-08-19
+     */
+    public function getPrivateKey()
+    {
+        if (!$this->xml)
+            return false;
+        return $this->xml->getElementsByTagName('RSASK')->item(0)->nodeValue;
+    }
+
+    /**
+     * Método que entrega la clave pública proporcionada por el SII para el CAF
+     * @return Clave pública en base64
+     * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
+     * @version 2015-08-19
+     */
+    public function getPublicKey()
+    {
+        if (!$this->xml)
+            return false;
+        return $this->xml->getElementsByTagName('RSAPUBK')->item(0)->nodeValue;
+    }
+
+}
