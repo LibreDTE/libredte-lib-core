6b0b31ce2973e0cd39633975bb10c040a97778b7
Esteban De La Fuente Rubio <esteban@delaf.cl>
2015-09-02 11:42:36 -0300

    Se agrega clase para trabajar con arreglos.

---
+++ lib/Arreglo.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * LibreDTE
+ * Copyright (C) SASCO SpA (https://sasco.cl)
+ *
+ * Este programa es software libre: usted puede redistribuirlo y/o
+ * modificarlo bajo los términos de la Licencia Pública General GNU
+ * publicada por la Fundación para el Software Libre, ya sea la versión
+ * 3 de la Licencia, o (a su elección) cualquier versión posterior de la
+ * misma.
+ *
+ * Este programa se distribuye con la esperanza de que sea útil, pero
+ * SIN GARANTÍA ALGUNA; ni siquiera la garantía implícita
+ * MERCANTIL o de APTITUD PARA UN PROPÓSITO DETERMINADO.
+ * Consulte los detalles de la Licencia Pública General GNU para obtener
+ * una información más detallada.
+ *
+ * Debería haber recibido una copia de la Licencia Pública General GNU
+ * junto a este programa.
+ * En caso contrario, consulte <http://www.gnu.org/licenses/gpl.html>.
+ */
+
+namespace sasco\LibreDTE;
+
+/**
+ * Clase para trabajar con Arreglos
+ * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
+ * @version 2015-08-20
+ */
+class Arreglo
+{
+
+    /**
+     * Une dos arreglos recursivamente manteniendo sólo una clave
+     * @param array $array1
+     * @param array $array2
+     * @return array
+     * @author Daniel <daniel (at) danielsmedegaardbuus (dot) dk>
+     * @author Gabriel Sobrinho <gabriel (dot) sobrinho (at) gmail (dot) com>
+     */
+    public static function mergeRecursiveDistinct(array $array1, array $array2)
+    {
+        $merged = $array1;
+        foreach ( $array2 as $key => &$value ) {
+            if (is_array($value) && isset($merged[$key]) && is_array($merged[$key])) {
+                $merged [$key] = self::mergeRecursiveDistinct(
+                    $merged [$key],
+                    $value
+                );
+            } else {
+                $merged [$key] = $value;
+            }
+        }
+        return $merged;
+    }
+
+}
