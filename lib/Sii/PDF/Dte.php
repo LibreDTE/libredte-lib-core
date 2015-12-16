ad06cc0d082d981944f394a3ffb3ef9c41e07b77
David Mancilla <dmancilla@gmail.com>
2015-12-16 12:48:54 -0300

    No mostrar texto de RazonRef cuando no existe, para no mostrar el punto y coma final

--- lib/Sii/PDF/Dte.php
+++ lib/Sii/PDF/Dte.php
@@ -631,7 +631,9 @@ class Dte extends \sasco\LibreDTE\PDF
         if (!isset($referencias[0]))
             $referencias = [$referencias];
         foreach($referencias as $r) {
-            $texto = $r['NroLinRef'].' - '.$this->getTipo($r['TpoDocRef']).' N° '.$r['FolioRef'].' del '.$r['FchRef'].': '.$r['RazonRef'];
+            $texto = $r['NroLinRef'].' - '.$this->getTipo($r['TpoDocRef']).' N° '.$r['FolioRef'].' del '.$r['FchRef'];
+			if (isset($r['RazonRef']))
+				$texto = $texto.': '.$r['RazonRef'];
             $this->Texto('Referenc.', $x);
             $this->Texto(':', $x+22);
             $this->MultiTexto($texto, $x+26);
@@ -657,7 +659,11 @@ class Dte extends \sasco\LibreDTE\PDF
 
         $this->setFont('', 'B', 5);
         foreach($referencias as $r) {
-            $texto = $r['NroLinRef'].' - '.$this->getTipo($r['TpoDocRef']).' N° '.$r['FolioRef'].' del '.$r['FchRef'].': '.$r['RazonRef'];
+            if (isset($r['RazonRef'])) {
+                $texto = $r['NroLinRef'].' - '.$this->getTipo($r['TpoDocRef']).' N° '.$r['FolioRef'].' del '.$r['FchRef'].': '.$r['RazonRef'];
+            } else {
+                $texto = $r['NroLinRef'].' - '.$this->getTipo($r['TpoDocRef']).' N° '.$r['FolioRef'].' del '.$r['FchRef'];
+            }
             $p1x = 3;
             $p1y   = $this->y+3;
             //$p2x   = $px2;
