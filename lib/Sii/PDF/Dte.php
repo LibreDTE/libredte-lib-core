f1a3e6f0438fc9a68a928332e86b002ee000d67b
Esteban De La Fuente Rubio <esteban@delaf.cl>
2016-08-23 01:26:39 -0300

    fix comuna receptor pdf

--- lib/Sii/PDF/Dte.php
+++ lib/Sii/PDF/Dte.php
@@ -563,7 +563,7 @@ class Dte extends \sasco\LibreDTE\PDF
      * @param receptor Arreglo con los datos del receptor (tag Receptor del XML)
      * @param x Posición horizontal de inicio en el PDF
      * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
-     * @version 2016-08-12
+     * @version 2016-08-23
      */
     private function agregarReceptor(array $receptor, $x = 10, $offset = 22)
     {
@@ -594,7 +594,9 @@ class Dte extends \sasco\LibreDTE\PDF
             $this->Texto('Dirección', $x);
             $this->Texto(':', $x+$offset);
             $this->setFont('', '', null);
-            $ciudad = !empty($receptor['CiudadRecep']) ? $receptor['CiudadRecep'] : \sasco\LibreDTE\Chile::getCiudad($receptor['CmnaRecep']);
+            $ciudad = !empty($receptor['CiudadRecep']) ? $receptor['CiudadRecep'] : (
+                !empty($receptor['CmnaRecep']) ? \sasco\LibreDTE\Chile::getCiudad($receptor['CmnaRecep']) : ''
+            );
             $this->MultiTexto($receptor['DirRecep'].(!empty($receptor['CmnaRecep'])?(', '.$receptor['CmnaRecep']):'').($ciudad?(', '.$ciudad):''), $x+$offset+2);
         }
         if (!empty($receptor['Extranjero']['Nacionalidad'])) {
