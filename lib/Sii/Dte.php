8e2d73d41cefede350679065023db2c93bd67038
Esteban De La Fuente Rubio <esteban@delaf.cl>
2018-05-22 20:30:18 -0400

    Se fuerza el uso de TpoTranVenta

--- lib/Sii/Dte.php
+++ lib/Sii/Dte.php
@@ -716,6 +716,10 @@ class Dte
                 ], $r);
             }
         }
+        // verificar que exista TpoTranVenta
+        if (!in_array($datos['Encabezado']['IdDoc']['TipoDTE'], [39, 41, 110, 111, 112]) and empty($datos['Encabezado']['IdDoc']['TpoTranVenta'])) {
+            $datos['Encabezado']['IdDoc']['TpoTranVenta'] = 1; // ventas del giro
+        }
     }
 
     /**
