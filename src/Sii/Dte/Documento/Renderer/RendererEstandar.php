a2f57d912b3565a1f5a344ca2cfc0ebd7213edab
Esteban De La Fuente Rubio <esteban@delaf.cl>
2024-12-21 11:24:11 -0300

    Fix tipo datos en atributo por herencia.

--- src/Sii/Dte/Documento/Renderer/RendererEstandar.php
+++ src/Sii/Dte/Documento/Renderer/RendererEstandar.php
@@ -34,5 +34,5 @@ class RendererEstandar extends AbstractRenderer
      *
      * @var string
      */
-    protected $defaultTemplate = 'estandar';
+    protected string $defaultTemplate = 'estandar';
 }
